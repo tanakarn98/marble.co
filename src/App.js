7f2dec29cc7e761100bc2dec023e710dd7900779
tanakarn98 <56716847+tanakarn98@users.noreply.github.com>
2020-11-24 02:51:26 +0900

    all complete!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
-import bgClouds from "./images/bgClouds.png";
-import topImages from "./images/topImages.png";
-import arrow from "./images/arrow.png";
+import topImage from "./images/topImage.png";
+import sTopImage from "./images/sTopImage.png";
+import mTopImage from "./images/mTopImage.png";
 import book from "./images/book.png";
 import bottomSnow from "./images/bottomSnow.png";
 import coffee from "./images/coffee.png";
@@ -15,28 +15,24 @@ import minibook from "./images/minibook.png";
 import originalCoffeeCan from "./images/original-coffee-can.jpg";
 import packageDesign from "./images/package-design.png";
 import topSnow from "./images/topSnow.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import marble_icon from "./images/Marble_icon.png";
 
 const App = () => {
-  const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-  for (let i = 0; i < smoothScrollTrigger.length; i++) {
-    smoothScrollTrigger[i].addEventListener("click", (e) => {
-      e.preventDefault();
-      let href = smoothScrollTrigger[i].getAttribute("href");
-      let targetElement = document.getElementById(href.replace("#", ""));
-      const rect = targetElement.getBoundingClientRect().top;
-      const offset = window.pageYOffset;
-      const gap = 60;
-      const target = rect + offset - gap;
-      window.scrollTo({
-        top: target,
-        behavior: "smooth",
-      });
-    });
-  }
+  window.onload = function () {
+    const spinner = document.getElementById("loading");
+    spinner.classList.add("loaded");
+  };
   return (
     <>
-      <img src={bgClouds} className="bgClouds" />
-      <img src={topImages} className="topImage"></img>
+      <div id="loading">
+        <img src={marble_icon} alt="marble_icon" />
+        <small>Loading...</small>
+      </div>
+      <div className="bgClouds"></div>
+      <img src={topImage} className="topImage" alt="topImage"></img>
+      <img src={sTopImage} className="sTopImage" alt="topImage"></img>
+      <img src={mTopImage} className="mTopImage" alt="topImage"></img>
 
       <a href="mailto:hello@marble-co.net?body=氏名、参加希望のワークショップ、参加人数をご記入の上送信してください。%0D%0A %0D%0A【氏名】%0D%0A【参加希望ワークショップ】%0D%0A【参加人数】%0D%0A %0D%0A 複数のワークショップに参加されたい場合は以下に同じ形式で続けてください。">
         <div className="alwaysTopRight">
@@ -45,21 +41,22 @@ const App = () => {
         </div>
       </a>
 
-      <div className="scroll">
-        <p>スクロール</p>
-        <img src={arrow}></img>
-      </div>
-
-      <a href="mailto:hello@marble-co.net?body=氏名、参加希望のワークショップ、参加人数をご記入の上送信してください。%0D%0A %0D%0A【氏名】%0D%0A【参加希望ワークショップ】%0D%0A【参加人数】%0D%0A %0D%0A 複数のワークショップに参加されたい場合は以下に同じ形式で続けてください。">
-        <button className="apply">
+      <a
+        href="mailto:hello@marble-co.net?body=氏名、参加希望のワークショップ、参加人数をご記入の上送信してください。%0D%0A %0D%0A【氏名】%0D%0A【参加希望ワークショップ】%0D%0A【参加人数】%0D%0A %0D%0A 複数のワークショップに参加されたい場合は以下に同じ形式で続けてください。"
+        className="apply"
+      >
+        <button>
           <i className="fas fa-envelope"></i>お申し込み
         </button>
       </a>
+
       <h2 className="aboutTitle">
-        こんにちは、<img src={Marbleco_logo}></img>です。
+        こんにちは、
+        <img src={Marbleco_logo} alt="Marble_logo" />
+        です。
       </h2>
       <div className="aboutFlex container">
-        <img src={packageDesign}></img>
+        <img src={packageDesign} alt="package_design"></img>
         <p>
           毎年200を超える印刷物をつくり、カフェやクライアントのお店のディスプレイをつくり、パッケージデザインをして、WEBもリリースしたり、日々さまざまなものをデザインしてカタチにしています。そんなわたしたちのデザインはどのようにして生み出されてきたのか。どうしてこんなデザインになったのか――　をデザイナーとともに追体験して、みなさんの日々の生活にデザインの視点を取り入れてもらえるような、そんな体験型ワークショップを開催します。
         </p>
@@ -73,35 +70,35 @@ const App = () => {
       <div className="container">
         <div className="trigger">
           <div className="workShops">
-            <a href="#kihon">
+            <AnchorLink href="#kihon">
               <div className="workShop book">
                 <p className="workShopCard kihon">印刷の基本の"キ"</p>
-                <img src={book}></img>
+                <img src={book} alt="book"></img>
               </div>
-            </a>
-            <a href="#can">
+            </AnchorLink>
+            <AnchorLink href="#can">
               <div className="workShop coffee">
                 <p className="workShopCard can">
-                  オリジナルコーヒー缶を
+                  オリジナル
                   <br />
-                  作ろう
+                  コーヒー缶を<nobr>作ろう</nobr>
                 </p>
-                <img src={coffee}></img>
+                <img src={coffee} alt="coffee"></img>
               </div>
-            </a>
-            <a href="#kaibo">
+            </AnchorLink>
+            <AnchorLink href="#kaibo">
               <div className="workShop glass">
                 <p className="workShopCard kaibo">
                   デザインを
                   <br />
                   解剖してみる
                 </p>
-                <img src={glass}></img>
+                <img src={glass} alt="glass"></img>
               </div>
-            </a>
+            </AnchorLink>
           </div>
 
-          <img src={topSnow} className="topSnow"></img>
+          <img src={topSnow} className="topSnow" alt="topSnow"></img>
           <div className="white">
             <div className="timeLine">
               <p className="schedule">
@@ -118,7 +115,7 @@ const App = () => {
             </div>
             <div>
               <div className="workShopFlex" id="kihon">
-                <img src={kihon_no_ki}></img>
+                <img src={kihon_no_ki} alt="kihon_no_ki"></img>
                 <div className="info">
                   <p className="workShopCard kihon2">
                     <nobr>印刷の基本の"キ"</nobr>
@@ -129,7 +126,7 @@ const App = () => {
                 </div>
               </div>
               <div className="workShopFlex" id="can">
-                <img src={originalCoffeeCan}></img>
+                <img src={originalCoffeeCan} alt="original_coffee_can"></img>
                 <div className="info">
                   <p className="workShopCard can2">
                     <nobr>オリジナルコーヒー缶を作ろう</nobr>
@@ -140,11 +137,11 @@ const App = () => {
                 </div>
               </div>
               <div className="workShopFlex" id="kaibo">
-                <img src={kaibo2}></img>
+                <img src={kaibo2} alt="kaibo2"></img>
                 <div className="info">
                   <p className="workShopCard kaibo2">
                     <nobr>デザインを解剖してみる</nobr>
-                    <img src={kaibo1} />
+                    <img src={kaibo1} alt="kaibo1" />
                   </p>
                   <p>
                     これまでマーブルが手がけてきた制作物を「どうしてこんなデザインになったのか？」をデザイナー本人と直接対話しながら掘り下げていきます。
@@ -161,14 +158,22 @@ const App = () => {
                 <p>
                   音を使った新感覚のゲームブックの体験コーナーも設置しています。体験版小冊子を会場にて配布中！（非売品）
                 </p>
-                <img src={minibook}></img>
+                <img src={minibook} alt="minibook"></img>
               </div>
             </div>
           </div>
-          <img src={bottomSnow} className="bottomSnow"></img>
+          <img src={bottomSnow} className="bottomSnow" alt="bottomSnow"></img>
           <div className="titleContainer">
-            <p className="title">参加方法</p>
+            <p className="title">概要 / 参加方法</p>
           </div>
+          <p className="center2">
+            【日時】2020.12.25(FRI)15:00 ~ 21:00 <br />
+            【会場】Marble BLDG.
+            <br />
+            【費用】3000円(コーヒー・プチケーキ付き)
+            <br />
+            【定員】各ワークショップ20人
+          </p>
           <p className="center">
             興味のある方はお気軽にお電話またはEmailにてお申し込みください。
             <br />
@@ -178,8 +183,11 @@ const App = () => {
               ※定員に達し次第締切とさせていただきます。
             </span>
           </p>
-          <a href="mailto:hello@marble-co.net?body=氏名、参加希望のワークショップ、参加人数をご記入の上送信してください。%0D%0A %0D%0A【氏名】%0D%0A【参加希望ワークショップ】%0D%0A【参加人数】%0D%0A %0D%0A 複数のワークショップに参加されたい場合は以下に同じ形式で続けてください。">
-            <button className="apply">
+          <a
+            href="mailto:hello@marble-co.net?body=氏名、参加希望のワークショップ、参加人数をご記入の上送信してください。%0D%0A %0D%0A【氏名】%0D%0A【参加希望ワークショップ】%0D%0A【参加人数】%0D%0A %0D%0A 複数のワークショップに参加されたい場合は以下に同じ形式で続けてください。"
+            className="apply"
+          >
+            <button>
               <i className="fas fa-envelope"></i>お申し込み
             </button>
           </a>
@@ -196,6 +204,8 @@ const App = () => {
               allowfullscreen=""
               aria-hidden="false"
               tabindex="0"
+              className="map"
+              title="marble_map"
             ></iframe>
             <div>
               <h4>Marble BLDG.</h4>
@@ -210,7 +220,7 @@ const App = () => {
           </div>
 
           <div className="titleContainer">
-            <p className="title">お問い合わせ / お申し込み</p>
+            <p className="title">お問い合わせ</p>
           </div>
           <div className="contact">
             <h4>Marble.co</h4>
@@ -228,12 +238,16 @@ const App = () => {
           </div>
         </div>
       </div>
-      <img src={bottomSnow} className="footerImage"></img>
+      <img src={bottomSnow} className="footerImage" alt="bottomSnow"></img>
       <div className="footer">
         <p>
           主催
-          <a href="https://www.marble-co.net/portfolio">
-            <img src={Marbleco_logo}></img>
+          <a
+            href="https://www.marble-co.net/portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Marbleco_logo} alt="marble_logo"></img>
           </a>
         </p>
       </div>
